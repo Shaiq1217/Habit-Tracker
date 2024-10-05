d193f864ff96b87db38459d326d06c86c279ca59
Shaiq1217 <shaiqi2001@gmail.com>
2024-10-05 18:35:25 +0800

    Router Fix

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,11 +4,11 @@ import errorHandler from "../common/error.js";
 import userRoutes from "./user.routes.js";
 import friendRoutes from "./friend.routes.js";
 import authRoutes from "./auth.routes.js";
-// import subhabitRoutes from "./subhabit.routes.js";
+import subhabitRoutes from "./subhabit.routes.js";
 const router = Router();
 
 router.use('/api/habits', habitsRoutes);
-// router.use('/api/subhabit', subhabitRoutes);
+router.use('/api/subhabit', subhabitRoutes);
 router.use('/api/user', userRoutes);
 router.use('/api/friends', friendRoutes);
 router.use('/api/auth', authRoutes);
